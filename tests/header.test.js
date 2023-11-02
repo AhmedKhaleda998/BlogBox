df4a9f8d9f62635b8ea71da089acdda0acf0eafb
Ahmed Khaled <77169294+AhmedKhaleda998@users.noreply.github.com>
2023-11-02 17:08:37 +0200

    Integration tests

---
+++ tests/header.test.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
+
+let browser, page;
+
+beforeEach(async () => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+    await browser.close();
+});
+
+test('the header has the correct text', async () => {
+    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    expect(text).toEqual('BlogBox');
+});
+
+test('clicking login starts oauth flow', async () => {
+    await page.click('.right a');
+    const url = await page.url();
+    expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('when signed in, shows logout button', async () => {
+    const user = await userFactory();
+    const { session, sig } = sessionFactory(user);
+
+    await page.setCookie({ name: 'session', value: session });
+    await page.setCookie({ name: 'session.sig', value: sig });
+
+    await page.goto('localhost:3000');
+
+    await page.waitFor('a[href="/auth/logout"]');
+
+    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    expect(text).toEqual('Logout');
+});
